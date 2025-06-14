216c176e7006d7da47a365db54485cbb7a1f5d15
oczih <arvo.matilainen@gmail.com>
2025-06-15 00:20:21 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use('/api/login', loginRouter)
 
 app.use(express.static(path.join(__dirname, 'dist')));
     
-app.use((req, res, next) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 
