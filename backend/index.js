4e96b06ed626a20c63406e0b6683f6fda66997a2
oczih <arvo.matilainen@gmail.com>
2025-06-15 00:08:07 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,29 @@ import cors from 'cors';
 import BooksRouter from './routes/books.js';
 import PeopleRouter from './routes/people.js';
 import loginRouter from './routes/login.js'
-import path from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+    
 app.use(express.json());
 app.use(cors())
 
 app.use('/api/books', BooksRouter);
 app.use('/api/people', PeopleRouter);
 app.use('/api/login', loginRouter)
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+    
+app.get('/*splat', (req, res) => {
+      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+}); 
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
@@ -32,6 +43,7 @@ mongoose.connect(MONGO_URI)
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error.message);
     })
+
 
 
 
